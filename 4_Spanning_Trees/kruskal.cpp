be2d4bcc8705fad8a544f60b4560a347228fd138
Saksham Sharma <saksham.adi@gmail.com>
2025-06-01 17:01:24 +0530

    new Kruskal implementation

--- 4_Spanning_Trees/kruskal.cpp
+++ 4_Spanning_Trees/kruskal.cpp
@@ -1,75 +1,119 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Disjoint{
-    int *link,*size;
-    int n;
+#define pb push_back
+///@brief
+/// Public Class to handle tuple data
+/// from--to (weight) per edge input
+/// current implmentation is non-directed
+class Edge
+{
+    public:
+        int from, to, wt;
+        Edge(){
 
-    Disjoint(int v)
-    {
-        this->n = v;
-        link = new int[n+1];
-        size = new int[n+1];
-        for(int i=0;i<=n;i++)
+        }
+        Edge(int f, int t, int w)
         {
-            link[i]=i;
-            size[i]=0;
+            this->from  = f;
+            this->to    = t;
+            this->wt    = w;
+        }
+        void display()
+        {
+            cout << "\t-> " << from << " - " << to << " (" << wt << ")\n";
+        }
+};
+///@brief union Find Class to handle
+/// disjoint set implmentation.
+class unionFind
+{
+public:
+    vector<int> parent;
+    vector<int> height;
+    unionFind(int size)
+    {
+        parent.resize(size);
+        height.resize(size);
+        for (int i = 0; i < size; i++)
+        {
+            parent[i] = i;
+            height[i] = 1;
         }
     }
-
-    int find(int x)
+    int find(int q)
     {
-        while(x!=link[x]) x=link[x];
-        return x;
+        if (parent[q] == q)
+            return q;
+        return find(parent[q]);
     }
-
-    void unite(int a,int b)
+    void unite(int a, int b)
     {
-        a=find(a);
-        b=find(b);
-        if(size[a]>size[b]) link[b]=a;
-        else link[a]=b;
-        if(size[a] == size[b]) size[b]++;
+        int aGrp = find(a);
+        int bGrp = find(b);
+        if (height[aGrp] > height[bGrp])
+        {
+            parent[aGrp] = bGrp;
+            height[bGrp] = height[bGrp] + height[aGrp];
+        }
+        else
+        {
+            parent[bGrp] = aGrp;
+            height[aGrp] = height[bGrp] + height[aGrp];
+        }
     }
 };
-int kruskal(vector<pair<int,pair<int,int>>> edges,int V)
+/// @brief
+/// Custom comparator for Edge class
+/// used to sort priority queue
+/// @return boolean
+bool compare(Edge &a, Edge &b)
 {
-    int mst_wt=0;
-
-    sort(edges.begin(),edges.end());
-
-    Disjoint ds(V);
-
-    vector<pair<int,pair<int,int>>>:: iterator i;
-    for(i=edges.begin();i!=edges.end();++i)
-    {
-        int s=i->second.first;
-        int e=i->second.second;
-
-        int linkS = ds.find(s);
-        int linkE = ds.find(e);
-
-        if(linkE!=linkS)
-        {
-            cout<<s<<" - "<<e<<'\n';
-            mst_wt=mst_wt+i->first;
-
-            ds.unite(linkE,linkS);
+    return a.wt < b.wt;
+}
+/// @brief Implementation of Kruskal's Minimum Spanning Tree algorithm
+/// @param adj - (Vector container with all the edges)
+/// @param verts - (INTEGER for number of vertices)
+/// @return (INTEGER) minDist (minimum MST weight)
+int kruskal(vector<Edge> &adj, int verts)
+{
+    unionFind uF(verts);
+    int minDist = 0;
+    sort(adj.begin(), adj.end(), compare);
+    for(int i=0 ; i<adj.size() ; i++){
+        int from= uF.find(adj[i].from);
+        int to  = uF.find(adj[i].to);
+        if(from != to){
+            uF.unite(from, to);
+            minDist = minDist + adj[i].wt;
         }
     }
-    return mst_wt;
+    return minDist;
+}
+/// @brief Main function running per test case
+/// @param void
+/// @return void
+void solve()
+{
+    int verts, edges;
+    cin >> verts >> edges;
+    vector<Edge> adj;
+    for (int i = 0; i < edges; i++)
+    {
+        int from, to, wt;
+        cin >> from >> to >> wt;
+        Edge tmp(from, to, wt);
+        adj.pb(tmp);
+    }
+    int minDist = kruskal(adj, verts);
+    cout << "MST weight : " << minDist << endl;
 }
 int main()
 {
-    int V,E;
-    cin>>V>>E;
-    vector<pair<int,pair<int,int>>> edges;
-    while(E--)
+    int tst;
+    cin >> tst;
+    while (tst--)
     {
-        int s,e,w;
-        cin>>s>>e>>w;
-        edges.push_back({w,{s,e}});
+        solve();
     }
-    int wt = kruskal(edges,V);
-    cout<<wt<<'\n';
     return 0;
 }
